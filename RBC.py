a1ae55baad887c6ef2c3f01bb7b02128a565334e
Shakeel-21 <sthibomt@gmail.com>
2024-04-26 08:58:38 +0000

    Added nextMovePrediction function

--- RBC.py
+++ RBC.py
@@ -12,21 +12,35 @@ def nextMovePrediction(line):
   possible_moves = list(board.pseudo_legal_moves) + [chess.Move.null()]
 
   if board.castling_rights:
-    temp_board = board.copy()
-    for move in utilities.without_opponent_pieces(temp_board).generate_castling_moves():
-      if not is_illegal_castle(temp_board, move):
-        possible_moves.append(move)
+    for move in utilities.without_opponent_pieces(board).generate_castling_moves():
+      possible_moves.append(move) if not is_illegal_castle(board, move) else None
 
   possible_moves = list(set(possible_moves))
-  possible_moves.sort(key=lambda move: move.uci())
-
-  for move in possible_moves:
+  for move in sorted(possible_moves, key=lambda move: move.uci()):
     print(move)
+
+
+def nextStatePrediction(line):
+    board = chess.Board(line)    
+    moves = list(board.pseudo_legal_moves) + [chess.Move.null()]
+    next_positions = []
+
+    if board.castling_rights:
+      for move in utilities.without_opponent_pieces(board).generate_castling_moves():
+        moves.append(move) if not is_illegal_castle(board, move) else None
+
+    for move in moves:       
+        temp_board = board.copy()
+        temp_board.push(move)        
+        next_positions.append(temp_board.fen()) 
+    next_positions.sort()
+    
+    for position in next_positions:
+        print(position)
 
 
 line=input()
 # boardRepresentation(line)
 # Call moveExecution here
-nextMovePrediction(line)
-
-
+# nextMovePrediction(line)
+nextStatePrediction(line)
